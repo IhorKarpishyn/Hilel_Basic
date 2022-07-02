604757db6c27da4db04a3b2c457f37c00fd1f005
Ihor Karpishyn <karpishyn.ihor@gmail.com>
2022-07-02 08:09:48 +0300

    Compleate Ex_15

---
+++ lesson_6/Ex_15.py
@@ -0,0 +1,41 @@
+'''
+Ваша задача — написать программу, которая переводит число во время в читабельном виде.
+Пользователь должен ввести число больше 0 и меньше 8639999.
+Число необходимо перевести в день, часы, минуты и секунды.
+Ну и дополнительной задачей является — забота о выводе.
+Слово "день" подбирается на основе кол-ва дней, а часы, минуты и секунды должны заполняться
+нулями при одноцифровых значениях.
+
+Пример:
+0 -> 0 дней, 00:00:00
+224930 -> 2 дня, 14:28:50
+466289 -> 5 дней, 09:31:29
+8639999 -> 99 дней, 23:59:59
+'''
+
+twentyfour_hours = {
+    'Day': 86400,
+    'Hour': 3600,
+    'Minute': 60,
+}
+day = {
+    'день': [1],
+    'дня': [2, 3, 4],
+    'дней': [0, 5, 6, 7, 8, 9]
+}
+
+final_dict = {}
+
+day_time = (int(input("Введите число от 0 до 8639999 : ")))
+if day_time in range(0, 8640000):
+    for key, value in twentyfour_hours.items():
+        final_dict[key] = (day_time // value)
+        day_time -= final_dict[key] * value
+    final_dict['Second'] = day_time
+for key, value in day.items():
+    for i in value:
+        if final_dict['Day'] % 10 == i:
+            dni = key
+print(final_dict['Day'], '{dni},'.format(dni=dni), '{:02}'.format(final_dict['Hour']), ':',
+      '{:02}'.format(final_dict['Minute']), ':',
+      '{:02}'.format(final_dict['Second']))
