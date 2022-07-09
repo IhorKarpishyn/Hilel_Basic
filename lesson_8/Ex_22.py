54fb5751038716e1a45204e1234b3023c4123157
Ihor Karpishyn <karpishyn.ihor@gmail.com>
2022-07-09 18:18:08 +0300

    Compleate Ex_22

---
+++ lesson_8/Ex_22.py
@@ -0,0 +1,25 @@
+"""
+Вам необходимо написать функцию, которая будет принимать список из чисел и находить среди них уникальное число.
+Пример:
+find_unique_value([1, 2, 1, 1]) -> 2
+find_unique_value([2, 3, 3, 3]) -> 2
+find_unique_value([5, 5, 5, 0.5]) -> 0.5
+"""
+
+# возвращаю уникальные из списка
+def find_unique_value(work_list):
+    unique = set(work_list)
+    unique_list = []
+    count = 0
+    for i in unique:
+        for y in work_list:
+            if i == y:
+                count += 1
+        if count < 2:
+            unique_list.append(i)
+        count = 0
+    return unique_list
+
+
+unique_numbers = find_unique_value([5, 5, 5, 0.5])
+print('Уникальные номера : ', *unique_numbers)
